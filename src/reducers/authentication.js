4f7cab45995a194ddea46bbb01cba6bf5377dc37
Daniele Salvatore <daniele.salvatore@fao.org>
2018-10-16 12:58:22 +0200

    clean up

--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -1,6 +1,5 @@
 import {LOGIN_ERROR, LOGIN_SUCCESS} from '../containers/Redirect/constants';
 import {LOGOUT_SUCCESS} from '../containers/Logout/constants';
-//import {FETCH_AUTH_USER_ERROR, FETCH_AUTH_USER_REQUEST, FETCH_AUTH_USER_SUCCESS} from '../containers/App/constants';
 
 const initialState = {};
 
@@ -8,19 +7,11 @@ function tokenReducer(state = initialState, action) {
 
     switch (action.type) {
         case LOGIN_SUCCESS:
-            return {...state, ...action.response.data, loading: false};
+            return {...state, ...action.response.data};
         case LOGIN_ERROR:
-            return {error: action.message, loading: false};
+            return {error: action.message};
         case LOGOUT_SUCCESS:
             return initialState;
-        // case FETCH_AUTH_USER_SUCCESS:
-        //     const entities = action.response.entities;
-        //     const id = action.response.result;
-        //     return {...entities.users[id], loading: false};
-        // case FETCH_AUTH_USER_ERROR:
-        //     return {error: action.message, loading: false};
-        // case FETCH_AUTH_USER_REQUEST:
-        //     return {loading: true};
         default:
             return state;
     }
